c27aafad163a9a8f92b6ab95eb279e2c155f175f
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-25 10:52:49 -0300

    Using exceptions

--- Ch2 - Basics/exceptions_start.py
+++ Ch2 - Basics/exceptions_start.py
@@ -5,10 +5,31 @@
 
 # Errors can happen in programs, and we need a clean way to handle them
 # TODO: This code will cause an error because you can't divide by zero:
+# x = 10 / 0
 
-# TODO: Exceptions provide a way of catching errors and then handling them in 
+# TODO: Exceptions provide a way of catching errors and then handling them in
 # a separate section of the code to group them together
-
+try:
+    x = 10 / 0
+except:
+    print("Well, that didn't work!")
 
 # TODO: You can also catch specific exceptions
+try:
+    x = "10" / 0
+except ZeroDivisionError:
+    print("Can't divide by zero now, can we?")
+except TypeError:
+    print("Well, that didn't work!")
 
+try:
+    answer = input("What should I divide 10 by? ")
+    num = int(answer)
+    print(10 / num)
+except ZeroDivisionError:
+    print("Can't divide by zero now, can we?")
+except ValueError as error:
+    print("Well, '{}' isn't a number now, is it?".format(answer))
+    print(error)
+finally:
+    print("This always runs")
