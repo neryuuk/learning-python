0531f185da4f949909360bc1003bd388cf8949af
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-25 16:20:32 -0300

    The date, time and datetime classes

--- Ch4 - Dates and Times/dates_start.py
+++ Ch4 - Dates and Times/dates_start.py
@@ -1,25 +1,29 @@
-#
-# Example file for working with date information
-# LinkedIn Learning Python course by Joe Marini
-#
-
-from datetime import date, time, datetime
+from datetime import date, datetime
 
 
 def main():
     # DATE OBJECTS
     # TODO: Get today's date from the simple today() method from the date class
     today = date.today()
-    print(today)
+    print("Today is {}".format(today))
 
     # TODO: print out the date's individual components
+    print("Date components:", today.day, today.month, today.year)
 
     # TODO: retrieve today's weekday (0=Monday, 6=Sunday)
+    print("Today's weekday # is {}".format(today.weekday()))
+    days = ["monday", "tuesday", "wednesday",
+            "thursday", "friday", "saturday", "sunday"]
+    print("Which is a {}".format(days[today.weekday()]))
 
     # DATETIME OBJECTS
     # TODO: Get today's date from the datetime class
+    today = datetime.now()
+    print("The current date and time is {}".format(today))
 
     # TODO: Get the current time
+    t = datetime.time(datetime.now())
+    print("The current time is {}".format(t))
 
 
 if __name__ == "__main__":
