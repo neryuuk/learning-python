fbf19fe1023d7d36903e73c9bc5e2002aca0eb32
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-27 10:51:15 -0300

    Fetching Internet data

--- Ch5 - Internet Data/inetdata_start.py
+++ Ch5 - Internet Data/inetdata_start.py
@@ -1,10 +1,18 @@
-# 
-# Example file for retrieving data from the internet
-# LinkedIn Learning Python course by Joe Marini
-#
+import urllib.request
+
 
 def main():
-    pass # this is a placeholder, do-nothing statement
+    # open a connection to a URL using urllib2
+    url = "https://google.com"
+    weburl = urllib.request.urlopen(url)
+
+    # get the result code and print it
+    print("GET {} {}".format(url, weburl.getcode()))
+
+    # read the data from the URL and print it
+    data = weburl.read()
+    print("PAYLOAD {}".format(data))
+
 
 if __name__ == "__main__":
     main()
